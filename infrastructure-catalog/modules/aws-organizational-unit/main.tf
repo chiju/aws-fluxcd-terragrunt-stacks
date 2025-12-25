4447340b5c147af25a9b3f97724f88551462f2fb
chiju <chijumelveettil@gmail.com>
2025-12-25 09:40:55 +0100

    fix: add terraform version and provider requirements to aws-organizational-unit module

--- infrastructure-catalog/modules/aws-organizational-unit/main.tf
+++ infrastructure-catalog/modules/aws-organizational-unit/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_organizations_organizational_unit" "ou" {
   name      = var.name
   parent_id = var.parent_id
