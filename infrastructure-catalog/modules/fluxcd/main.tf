b8e7fde33bf975c352fa0f0f8d6631b944e506b6
chiju <chijumelveettil@gmail.com>
2025-12-27 18:27:23 +0100

    Fix Helm provider syntax - use values instead of set blocks

--- infrastructure-catalog/modules/fluxcd/main.tf
+++ infrastructure-catalog/modules/fluxcd/main.tf
@@ -13,15 +13,16 @@ resource "helm_release" "flux_operator" {
   namespace        = "flux-system"
   create_namespace = true
 
-  set {
-    name  = "livenessProbe.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "readinessProbe.enabled"
-    value = "false"
-  }
+  values = [
+    yamlencode({
+      livenessProbe = {
+        enabled = false
+      }
+      readinessProbe = {
+        enabled = false
+      }
+    })
+  ]
 
   depends_on = [data.aws_eks_cluster.cluster]
 }
