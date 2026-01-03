db9c448e91d90951b4794ffdfbe162212c99bc5e
chiju <chijumelveettil@gmail.com>
2026-01-03 16:58:34 +0100

    fix: remove unused variables from fluxcd module
    - Remove aws_region and account_id variables (no longer used)
    - Fixes TFLint terraform_unused_declarations warnings
    - Clean up after removing CRD wait script

--- infrastructure-catalog/modules/fluxcd/variables.tf
+++ infrastructure-catalog/modules/fluxcd/variables.tf
@@ -9,18 +9,6 @@ variable "cluster_endpoint" {
   default     = null
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "account_id" {
-  description = "AWS account ID"
-  type        = string
-}
-
-
-
 variable "namespace" {
   description = "Kubernetes namespace for FluxCD"
   type        = string
